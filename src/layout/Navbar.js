8eeea7327ade57ae368113b95fe492f5b6459db7
Alex <vadean.flaviu.alex@gmail.com>
2023-09-14 20:30:33 +0300

    Add files via upload

---
+++ src/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+                <h1>Navbar</h1>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <Link className='btn btn-outline-light' to='/adduser'>Add user</Link>
+            </nav>
+        </div>
+    )
+}
